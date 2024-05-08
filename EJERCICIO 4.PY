3b400a99d8a9193ec1150548043cdd2c99585293
romerolucas1 <152455204+romerolucas1@users.noreply.github.com>
2024-05-08 11:17:14 -0300

    Create EJERCICIO 4.PY

---
+++ EJERCICIO 4.PY
@@ -0,0 +1,5 @@
+# Define una tupla llamada colores que contenga los colores 'rojo', 'verde' y 'azul'.
+colores = ('rojo', 'verde', 'azul')
+
+# Imprime el segundo color de la tupla.
+print("El segundo color de la tupla es:", colores[1])
