94a25fa4ef678ca40cbbfd7220a4f3a9198b4667
romerolucas1 <152455204+romerolucas1@users.noreply.github.com>
2024-05-08 11:15:53 -0300

    Create EJERCICIO 3.PY

---
+++ EJERCICIO 3.PY
@@ -0,0 +1,8 @@
+# Define una lista llamada numeros que contenga los números del 1 al 5.
+numeros = [1, 2, 3, 4, 5]
+
+# Agrega el número 6 al final de la lista.
+numeros.append(6)
+
+# Imprime la lista resultante.
+print("Lista de números:", numeros)
