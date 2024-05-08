1dfc90f09ff1ed71ca29c7331cf712c151346296
romerolucas1 <152455204+romerolucas1@users.noreply.github.com>
2024-05-08 11:17:54 -0300

    Create EJERCICIO 5.PY

---
+++ EJERCICIO 5.PY
@@ -0,0 +1,18 @@
+# Define un diccionario llamado datos_personales que contenga los datos de una persona.
+datos_personales = {
+    'nombre': 'Juan',
+    'edad': 25,
+    'ciudad': 'Ciudad de México'
+}
+
+# Imprime el nombre de la persona.
+print("Nombre de la persona:", datos_personales['nombre'])
+
+# Actualiza la edad de la persona a 30 años.
+datos_personales['edad'] = 30
+
+# Agrega el dato de género con el valor 'masculino'.
+datos_personales['genero'] = 'masculino'
+
+# Imprime el diccionario actualizado.
+print("Diccionario actualizado:", datos_personales)
